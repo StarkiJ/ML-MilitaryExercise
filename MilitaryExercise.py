043ce2ef26b9cbb50399be92ce4367ef7211bfef
StarkiJ <132117607+StarkiJ@users.noreply.github.com>
2024-06-16 03:35:30 +0800

    pass by blue

--- MilitaryExercise.py
+++ MilitaryExercise.py
@@ -236,7 +236,7 @@ class MilitaryExercise:
             x, y, steps = queue.popleft()
 
             # 检查燃料是否充足
-            if fuel < steps or change > 3:
+            if fuel < steps or change > 1:
                 if aim == 0:
                     if target[2] < 0:
                         if self.fighters[fid].max_fuel < steps:
@@ -348,10 +348,10 @@ class MilitaryExercise:
     # 该指令表示战斗机寻找目标。第一个参数为战斗机编号。
     def find_target(self, fid):
         fight = self.fighters[fid]
-        # # 抵达蓝色基地则加油和补充弹药
-        # if self.map_info[fight.row][fight.col] == "*":
-        #     self.flue(fid, fight.max_fuel)
-        #     self.missile(fid, fight.max_missile)
+        # 路过无预约的蓝色基地则加油和补充弹药
+        if self.map_info[fight.row][fight.col] == "*" and (fight.row, fight.col) not in self.targets:
+            self.flue(fid, fight.max_fuel)
+            self.missile(fid, fight.max_missile)
         # 是否待命
         if not (self.targets[fid] == (-1, -1)):
             return
