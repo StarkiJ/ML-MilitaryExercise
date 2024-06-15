1f672d04585695eabc0e22bc7106ee8d100400ff
StarkiJ <132117607+StarkiJ@users.noreply.github.com>
2024-06-16 05:00:32 +0800

    nothing

--- main.py
+++ main.py
@@ -7,34 +7,32 @@ from machine_operation import machine_operation
 
 
 def main():
-    for i in range(1, 10):
-        print('./data2/testcase' + str(i) + '.in')
-        me = MilitaryExercise('./data2/testcase' + str(i) + '.in')
-        me.show_info()
+    me = MilitaryExercise('./data2/testcase10.in')
+    me.show_info()
 
-        print("Please choose a start mode:")
-        print("1. Manual operation")
-        print("2. Machine operation")
-        print("3. Machine learning")
-        print("q. Quit")
+    print("Please choose a start mode:")
+    print("1. Manual operation")
+    print("2. Machine operation")
+    print("3. Machine learning")
+    print("q. Quit")
 
-        while True:
-            # choice = input("Enter choice: ")
-            choice = '2'
-            if choice == '1':
-                manual_operation(me)
-                break
-            elif choice == '2':
-                machine_operation(me)
-                break
-            elif choice == '3':
-                # machine_learning()
-                break
-            elif choice == 'q':
-                print("Exiting the program.")
-                break
-            else:
-                print("Invalid choice.")
+    while True:
+        # choice = input("Enter choice: ")
+        choice = '2'
+        if choice == '1':
+            manual_operation(me)
+            break
+        elif choice == '2':
+            machine_operation(me)
+            break
+        elif choice == '3':
+            # machine_learning()
+            break
+        elif choice == 'q':
+            print("Exiting the program.")
+            break
+        else:
+            print("Invalid choice.")
 
 
 if __name__ == "__main__":
