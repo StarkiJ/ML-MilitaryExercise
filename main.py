c7ef2bd5ef5251f1d1c3fd737e565153aadeb0c3
StarkiJ <132117607+StarkiJ@users.noreply.github.com>
2024-06-16 21:00:51 +0800

    auto_main

--- main.py
+++ main.py
@@ -11,11 +11,44 @@ def main():
     input_path = './data2/testcase' + test_num + '.in'
     output_path = './data2/testcase' + test_num + '.out'
 
-    me = MilitaryExercise(input_path)
-    me.show_info()
+    # print("Please choose a start mode:")
+    # print("1. Manual operation")
+    # print("2. Machine operation")
+    # print("3. Machine learning")
+    # print("q. Quit")
 
-    machine_operation(me, output_path)
+    while True:
+        # choice = input("Enter choice: ")
+        choice = '2'
+
+        me = MilitaryExercise(input_path)
+        me.show_info()
+
+        if choice == '1':
+            manual_operation(me)
+            break
+        elif choice == '2':
+            machine_operation(me, output_path)
+            break
+        elif choice == '3':
+            # machine_learning()
+            break
+        elif choice == 'q':
+            print("Exiting the program.")
+            break
+        else:
+            print("Invalid choice.")
+
+
+def auto_main():
+    for i in range(1, 11):
+        test_num = str(i)
+        input_path = './data2/testcase' + test_num + '.in'
+        output_path = './data2/testcase' + test_num + '.out'
+        me = MilitaryExercise(input_path)
+        me.show_info()
+        machine_operation(me, output_path)
 
 
 if __name__ == "__main__":
-    main()
+    auto_main()
