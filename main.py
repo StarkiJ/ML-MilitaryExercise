75b6da489eeeb4d714e7c2aca7014b22e675c5f0
StarkiJ <132117607+StarkiJ@users.noreply.github.com>
2024-06-16 20:57:54 +0800

    auto_main

--- main.py
+++ main.py
@@ -38,5 +38,15 @@ def main():
             print("Invalid choice.")
 
 
+def auto_main():
+    for i in range(1, 11):
+        test_num = str(i)
+        input_path = './data2/testcase' + test_num + '.in'
+        output_path = './data2/testcase' + test_num + '.out'
+        me = MilitaryExercise(input_path)
+        me.show_info()
+        machine_operation(me, output_path)
+
+
 if __name__ == "__main__":
-    main()
+    auto_main()
